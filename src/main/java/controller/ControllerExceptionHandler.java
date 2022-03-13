4ad88f83d6b206e9fc4adaa03f3690072a18fd20
Ronan <ronan.poinsignon@laposte.net>
2022-03-13 07:30:34 +0100

    modif status erreur sur tooManyRequests + modif gestion retour erreur

--- src/main/java/controller/ControllerExceptionHandler.java
+++ src/main/java/controller/ControllerExceptionHandler.java
@@ -1,11 +1,17 @@
 package controller;
 
+import java.util.Date;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import exception.TooManyRequestsException;
+import prog.exceptions.NoVideoFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -17,8 +23,58 @@ public class ControllerExceptionHandler {
 	}
 
 	@ExceptionHandler(value = {TooManyRequestsException.class})
-	@ResponseStatus(HttpStatus.FORBIDDEN)
-	public String internalServerError(TooManyRequestsException ex) {
-		return ex.getMessage();
+	public ResponseEntity<ErrorResponse> internalServerError(TooManyRequestsException ex) {
+		return buildError(ex, HttpStatus.TOO_MANY_REQUESTS);
+	}
+
+	private ResponseEntity<ErrorResponse> buildError(Exception ex, HttpStatus status){
+		return new ResponseEntity<>(new ErrorResponse(status, ex.getMessage()), status);
+	}
+
+	private class ErrorResponse {
+
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+		private Date timestamp;
+
+		private int code;
+
+		private String status;
+
+		private String message;
+
+		public ErrorResponse() {
+			timestamp = new Date();
+		}
+
+		public ErrorResponse(
+				HttpStatus httpStatus,
+				String message
+				) {
+			this();
+
+			code = httpStatus.value();
+			status = httpStatus.name();
+			this.message = message;
+		}
+
+		@SuppressWarnings("unused")
+		public Date getTimestamp() {
+			return timestamp;
+		}
+
+		@SuppressWarnings("unused")
+		public int getCode() {
+			return code;
+		}
+
+		@SuppressWarnings("unused")
+		public String getStatus() {
+			return status;
+		}
+
+		@SuppressWarnings("unused")
+		public String getMessage() {
+			return message;
+		}
 	}
 }
